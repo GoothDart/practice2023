1652b9f595932f85a224ef406dee2372d4437f7d
GoothDart <dan.v.b.2005@gmail.com>
2023-07-08 18:16:52 +0300

    коммит 2

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ flagerror = False
 log = open('log/prebuf_resynth.log')
 
 while True:
+
     line = log.readline().strip()
+
     if not line:
-        break
         fldict['Error'] = flagerror
+        break
+
     if ('ERROR' in line) or ('FATAL' in line):
         flagerror = True
 
