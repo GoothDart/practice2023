0245424b1c369363780ca31e46461f611362d837
GoothDart <dan.v.b.2005@gmail.com>
2023-07-09 02:07:11 +0300

    commit 4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 #flnamedict = dict()
 #flslackdict = dict()
@@ -11,7 +12,8 @@ flagerror = False
 logpath = 'log/prebuf_resynth.log'
 log = open(logpath)
 path = Path(logpath)
-
+date = ''
+slack = []
 
 while True:
 
@@ -20,11 +22,21 @@ while True:
     if not line:
         fldict['Filename'] = path.name
         fldict['Error'] = flagerror
+        fldict['Date'] = date
         break
 
     if ('ERROR' in line) or ('FATAL' in line):
         flagerror = True
 
+    if (re.search(r'[Ss]lack', line)):
+        continue
+    
+    if (re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line)):
+        span = re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line).span()
+        line = line[span[1]:]
+        span = re.search(r'\w', line).span()
+        line = line[span[0]:]
+        date = line
 
 print(fldict)
 
