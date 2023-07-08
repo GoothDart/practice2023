9a2929fe8a16ffa56af3ea9c64fe2121c624a253
GoothDart <dan.v.b.2005@gmail.com>
2023-07-08 14:58:36 +0300

    коммит 1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-a = 10
+#flnamedict = dict()
+#flslackdict = dict()
+#fldatedict = dict()
+#flerrordict = dict()
+
+fldict = dict()
+
+flagerror = False
+log = open('log/prebuf_resynth.log')
+
+while True:
+    line = log.readline().strip()
+    if not line:
+        break
+        fldict['Error'] = flagerror
+    if ('ERROR' in line) or ('FATAL' in line):
+        flagerror = True
+
+
+print(fldict)
+
+
