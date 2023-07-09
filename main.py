e2b2ec7e805e91975a8272ae66aea89548781e5a
GoothDart <dan.v.b.2005@gmail.com>
2023-07-10 01:52:28 +0300

    commit 5

--- main.py
+++ main.py
@@ -1,43 +1,77 @@
 from pathlib import Path
 import re
+import os
 
 #flnamedict = dict()
 #flslackdict = dict()
 #fldatedict = dict()
 #flerrordict = dict()
 
-fldict = dict()
+#fldict = dict()
 
-flagerror = False
-logpath = 'log/prebuf_resynth.log'
-log = open(logpath)
-path = Path(logpath)
-date = ''
-slack = []
+listlog = os.listdir('log')
+#print(listlog)
 
-while True:
+#flagerror = False
+#logpath = 'log/prebuf_resynth.log'
+#log = open(logpath)
+#path = Path(logpath)
+#date = ''
+#slack = []
 
-    line = log.readline().strip()
+for logpath in listlog:
 
-    if not line:
-        fldict['Filename'] = path.name
-        fldict['Error'] = flagerror
-        fldict['Date'] = date
-        break
-
-    if ('ERROR' in line) or ('FATAL' in line):
-        flagerror = True
-
-    if (re.search(r'[Ss]lack', line)):
+    if Path(logpath).suffix != '.log':
         continue
-    
-    if (re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line)):
-        span = re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line).span()
-        line = line[span[1]:]
-        span = re.search(r'\w', line).span()
-        line = line[span[0]:]
-        date = line
 
-print(fldict)
+    fldict = dict()
+
+    flagerror = False
+    logpath = 'log/' + logpath
+    log = open(logpath)
+    path = Path(logpath)
+    date = ''
+    slack = []
+    flagdate = False
+
+    #для отладки
+    print(path.name)
+
+    while True:
+
+        line = log.readline().strip()
+
+        if not line:
+            fldict['Filename'] = path.name
+            fldict['Error'] = flagerror
+            fldict['Date'] = date
+            break
+        
+        #часть проверки времени, случай если время указано строчкой ниже
+        if flagdate:
+            date = line
+            flagdate = False
+
+        #поиск ошибки
+        if ('ERROR' in line) or ('FATAL' in line):
+            flagerror = True
+
+        #поиск slack
+        if (re.search(r'[Ss]lack', line)):
+            continue
+    
+        #ищет время и выдает срез строки после найденных слов и когда появляется первая буква\цифра. span - кортеж из начала и конца найденного соответствия(индексы)
+        if (re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line)):
+            span = re.search(r'([dD]ate)|([Ss]tart [tT]ime)', line).span()
+            line = line[span[1]:]
+            span = re.search(r'\w', line)
+            if span:
+                span = span.span()
+                line = line[span[0]:]
+                date = line
+            else:
+                flagdate = True
+
+    print(fldict)
 
 
