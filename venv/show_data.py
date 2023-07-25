436046bcbfeb6f2cb3354af5a3d8e110ffd0481c
GoothDart <dan.v.b.2005@gmail.com>
2023-07-26 01:08:52 +0300

    commit 14

--- venv/show_data.py
+++ venv/show_data.py
@@ -9,11 +9,11 @@ def get_data():
     try:
         connection = mariadb.connect(user = 'Wslacklog_user', password = 'Wslacklog', database = 'Wslacklog_db', host = 'localhost', port = '3306')
         query = f"SELECT `id`, `file_name` FROM `file`"
-        print('!!!!!!!!!!!!')
         print(query)
         cursor = connection.cursor(buffered=True)
         cursor.execute(query)
         id_file = cursor.fetchall()
+        print(id_file)
 
         slerror = []
         sldate = []
@@ -25,19 +25,48 @@ def get_data():
             cursor.execute(query)
             error_detected = cursor.fetchone()
             slerror.append(error_detected)
+            print(query)
 
             query = f"SELECT `start_date` FROM `dates` WHERE `id_file` = {idf[0]}"
             cursor.execute(query)
             start_date = cursor.fetchone()
             sldate.append(start_date)
+            print(query)
 
             query = f"SELECT `worst_slack` FROM `slacks` WHERE `id_file` = {idf[0]}"
             cursor.execute(query)
             worst_slack = cursor.fetchone()
             slslack.append(worst_slack)
+            print(query)
 
-            slslack.append(idf[1])
+            slfile.append(idf[1])
 
         connection.close()
+        print('slerror: ', slerror)
+        print('sldate: ', sldate)
+        print('slslack: ', slslack)
+        print('slfile: ', slfile)
+
+        # print('<!doctype html>')
+# print('<html>')
+# print('<head>')
+# print('<meta charset="utf-8">')
+# print('<title>wslack</title>')
+# print('</head>')
+# print('<body>')
+# print('<table border="1">')
+# #print('<!--<caption></caption>-->')
+# print('<tr>')
+# print('<th>file_name</th>')
+# print('<th>date</th>')
+# print('<th>error</th>')
+# print('<th>slack</th>')
+# print('</tr>')
+# for i in range(len(slfile)):
+#     print(f'<tr><td>{slfile[i]}</td><td>{sldate[i]}</td><td>{slerror[i]}</td><td>{slslack[i]}</td></tr>')
+# print('</table>')
+# print('</body>')
+
     except Error as error:
         print(f'Ошибка подключения к БД: {error}')
+get_data()
